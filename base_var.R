5483cc3d044a5dd203b0e7d86cae4684c3e94753
yorkyi <yiyusheng.hust@gmail.com>
2017-04-21 17:24:56 +0800

    0421:commit

--- base_var.R
+++ base_var.R
@@ -1,5 +1,5 @@
 ###### VARIABLES ######
-dirName <- 'Disk_Workload_201406-201407'
+dirName <- 'Load_Data_2015'
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#CC6666", "#9999CC", "#66CC99","#000000")
 attrNameAll <- c('util','rps','iopsr','wps','iopsw');attrNameDis <- attrNameAll[c(2,4,1)]
 dir_code <- paste(dir_c,dirName,sep='')
@@ -9,8 +9,10 @@ if (osFlag){
   source('D:/Git/R_libs_user/R_custom_lib.R')
 }else{
   dir_data15 <- '/home/yiyusheng/Data/Load_Data_2015/mergePartSvrid/'
-  dir_data15D <- '/home/yiyusheng/Data/Load_Data_2015/mergePartSvridDcast/'
-  dir_data15DC <- '/home/yiyusheng/Data/Load_Data_2015/mergePartSvridDcastClear/'
+  dir_data15AD <- '/home/yiyusheng/Data/Load_Data_2015/attridDcast/'
+  dir_data15ADC <- '/home/yiyusheng/Data/Load_Data_2015/attridDcastClear/'
+  dir_data15ADRC <- '/home/yiyusheng/Data/Load_Data_2015/attridDcastReduceClear/'
+  
   dir_data14D <- '/home/yiyusheng/Data/Load_Data_2014/merge_1k/'
   dir_data14DC <- '/home/yiyusheng/Data/Load_Data_2014/merge_1kClear/'
   dir_data14 <- '/home/yiyusheng/Data/Load_Data_2014/merge_5k/'
